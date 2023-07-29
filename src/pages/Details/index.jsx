82976d56be110f8cae2c7599f3eac7f1cc05b4c1
lucasluiz19 <lucas844@outlook.com.br>
2023-07-28 21:03:26 -0300

    update section links

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,26 @@
-import {Container} from "./style"
+import {Container, Links} from "./style"
 import { Button } from "../../components/button"
 import { Header} from "../../components/header"
-
+import { Section } from "../../components/Section"
 export function Details(){
   return(
  //no return so podemos ter 1 elemento por isso podemos usar o fragment ou uma div   
     <Container>  
       
       <Header/>
-      
+
+      <Section title="links úteis">
+        <Links>
+          <li>
+            <a href="#">https://rocketseat.com</a>
+          </li>
+          <li>
+            <a href="#">https://rocketseat.com</a>
+          </li>
+          
+        </Links>
+      </Section>
+
       <Button title="voltar" />
 
     </Container>
